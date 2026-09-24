679205a171672a8cefe932f0ea0542c7101d0fe0
Malcolm Matalka <malcolm.matalka@acsl.se>
2026-09-24 11:22:26 +0200

    failed_check_recreated_on_push_0013

--- tf/tf.tf
+++ tf/tf.tf
@@ -1,2 +1,9 @@
 resource "null_resource" "foo" {
 }
+
+# Fails when it is applied, after the plan succeeded, so that the apply reports a failed dirspace.
+resource "null_resource" "fail" {
+  provisioner "local-exec" {
+    command = "exit 1"
+  }
+}
